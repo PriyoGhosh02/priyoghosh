503d3796c6afd2b61a1c4f5f6c8b612c634de390
Priyo ghosh <128085549+PriyoGhosh02@users.noreply.github.com>
2026-05-03 18:20:16 +0600

    Delete dist directory

--- dist/server/assets/_tanstack-start-manifest_v-D9-BUgLi.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/dev/ReactJSPortfolio/src/routes/__root.tsx", children: ["/", "/api/chat"], assets: void 0, preloads: ["/assets/index-BSflCf4U.js"] }, "/": { filePath: "D:/dev/ReactJSPortfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-C2L4HVRK.js"] } }, clientEntry: "/assets/index-BSflCf4U.js" });
-export {
-  tsrStartManifest
-};
